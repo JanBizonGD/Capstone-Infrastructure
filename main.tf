efde53519d18ecc54a650a28c3d266a6693d9f83
Jan Bizoń <jbizon@griddynamics.com>
2025-04-15 13:48:38 +0200

    sensitive

--- main.tf
+++ main.tf
@@ -50,9 +50,11 @@ resource "azurerm_container_registry" "acr" {
 }
 output "acr_username" {
   value = azurerm_container_registry.acr.admin_username
+  sensitive = true
 }
 output "acr_password" {
   value = azurerm_container_registry.acr.admin_password
+  sensitive = true
 }
 
 
