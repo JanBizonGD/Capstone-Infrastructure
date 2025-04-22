dcdaeb6d4c82ecfbd8fc473631ce5ac7f8d47fd2
Jan Bizoń <jbizon@griddynamics.com>
2025-04-22 15:37:31 +0200

    sql

--- main.tf
+++ main.tf
@@ -231,3 +231,32 @@ resource "azurerm_network_security_group" "nsg" {
     }
 }
 
+
+# Database
+resource "azurerm_mssql_server" "my_sql_server" {
+  name                         = "petclinic-sqlserver"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "azureuser"
+  administrator_login_password = "Password123!"
+}
+
+resource "azurerm_mssql_database" "my_sql_database" {
+  name         = "petclinic-db"
+  server_id    = azurerm_mssql_server.my_sql_server.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+  enclave_type = "VBS"
+
+  tags = {
+    foo = "bar"
+  }
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = true
+  }
+}
