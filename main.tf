58840fcb69f93201d6826bce29aecfc296d658bc
Jan Bizoń <jbizon@griddynamics.com>
2025-04-22 14:47:50 +0200

    array

--- main.tf
+++ main.tf
@@ -152,7 +152,7 @@ data "azurerm_lb_backend_address_pool" "vmss_nics" {
   loadbalancer_id = azurerm_lb.example_lb.id
 }
 output "private_ips" {
-  value = [ data.azurerm_lb_backend_address_pool.vmss_nics.backend_address.ip_address ]
+  value = [ data.azurerm_lb_backend_address_pool.vmss_nics.backend_address[*].ip_address ]
 }
 
 
