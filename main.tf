fa2df6abfb2ecb5d93a62496bb00c32740424673
Jan Bizoń <jbizon@griddynamics.com>
2025-04-23 13:15:18 +0200

    typos

--- main.tf
+++ main.tf
@@ -155,13 +155,13 @@ data "azurerm_lb_backend_address_pool" "vmss_nics" {
 
 data "azurerm_virtual_machine_scale_set" "vmss" {
   name = azurerm_linux_virtual_machine_scale_set.vmss.name
-  resource_group_name = zurerm_linux_virtual_machine_scale_set.vmss.resource_group_name
+  resource_group_name = azurerm_linux_virtual_machine_scale_set.vmss.resource_group_name
 
   depends_on = [ azurerm_linux_virtual_machine_scale_set.vmss ]
 }
 
 output "private_ips" {
-  value = data.azurerm_linux_virtual_machine_scale_set.vmss.instances.*.private_ip_address
+  value = data.azurerm_virtual_machine_scale_set.vmss.instances.*.private_ip_address
   //value = [ data.azurerm_lb_backend_address_pool.vmss_nics.backend_address[*].ip_address ]
 }
 
